1ea8d9241ef10081a7b6c57d83716eecc6b37477
shiva9520 <kashyapshiva073@gmail.com>
2026-01-18 16:36:02 +0530

    update resume url

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1Cp4n6AfQQCUkccx76ZZk7hfQ9_xkTEP"
+                href="https://drive.google.com/file/d/1Cp4n6AfQQCUkccx76ZZk7hfQ9_xkTEP-/view?usp=sharing"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
